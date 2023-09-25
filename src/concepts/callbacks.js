9fc091bb365f7bbd924de10898d123337883f9d3
BettoR <franrod1082@gmail.com>
2023-09-25 15:11:24 -0400

    Se agrega manejor de errores en callback.js

--- src/concepts/callbacks.js
+++ src/concepts/callbacks.js
@@ -7,18 +7,30 @@ import { heroes } from "../data/heroes";
 export const callbacksComponent = ( element ) => {
     
     const id = '5d86371f25a058e5b1c8a65e';
-    findHero( id, (hero) => {
-        element.innerHTML = hero.name
+    findHero( id, (error, hero) => {
+       // element.innerHTML = hero?.name;
+       if ( error ) {
+        element.innerHTML = error;
+        return;
+       }
+
+       element.innerHTML = hero.name;
     });
 }
 
 /**
  * 
  * @param {String} id 
- * @param {(hero:Object)=> void} callback 
+ * @param {error: String|Null(hero:Object)=> void} callback 
  */
 const findHero = (id, callback) => {
    const hero = heroes.find( hero => hero.id === id );
 
-   callback( hero );
+   if ( !hero ) {
+    callback(`Hero with id ${ id } not found`);
+    return;
+   }
+
+
+   callback( null, hero );
 }
